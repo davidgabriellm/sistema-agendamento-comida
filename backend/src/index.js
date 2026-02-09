3c74ef128947d4b11c2220468cc043054c391207
davidgabriellm <davidglm.2002@gmail.com>
2026-02-09 09:49:05 -0300

    feat(auth): implement JWT authentication and authorization

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,20 @@
 import express from 'express';
 import cors from 'cors'
+import { Mongo } from './database/mongo.js'
+import {config} from 'dotenv'
+import authRouter from './auth/auth.js';
 
+config()
 
 async function main () {
     const hostname = 'localhost'
     const port = 3000
 
     const app = express()
+
+    const mongoConnection = await Mongo.connect({ mongoConnectionString: process.env.MONGO_CS, mongoDbName: process.env.MONGO_DB_NAME })
+    console.log(mongoConnection)
+    
 
     
     app.use(express.json())
@@ -19,6 +27,7 @@ async function main () {
             body: 'Welcome to MyGastronomy!'
         })
     })
+    app.use('/auth', authRouter)
 
     
     app.listen(port, () => {
